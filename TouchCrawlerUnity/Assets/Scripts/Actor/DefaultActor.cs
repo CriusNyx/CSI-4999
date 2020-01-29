8de86a128e81d9674076ba554ae913499313a959
zzweber1 <35668141+zzweber1@users.noreply.github.com>
2020-01-29 14:18:54 -0500

    Fixes to Default Actor

--- TouchCrawlerUnity/Assets/Scripts/Actor/DefaultActor.cs
+++ TouchCrawlerUnity/Assets/Scripts/Actor/DefaultActor.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class DefaultActor : MonoBehaviour, IActor
 {
+    public int actorLevel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    public IActor target { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
+    public MovementController mc => throw new System.NotImplementedException();
+
+    public bool IsPlayer()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void PickUpItem(object item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UseItem(object item)
+    {
+        throw new System.NotImplementedException();
+    }
 }
