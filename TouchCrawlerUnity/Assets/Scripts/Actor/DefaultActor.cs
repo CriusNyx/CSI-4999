82cd25b08a958ee1440ff625c35abcb90c1c7349
zzweber1 <35668141+zzweber1@users.noreply.github.com>
2020-01-30 16:11:13 -0500

    Movement for player

--- TouchCrawlerUnity/Assets/Scripts/Actor/DefaultActor.cs
+++ TouchCrawlerUnity/Assets/Scripts/Actor/DefaultActor.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Events;
 using UnityEngine;
 
-public class DefaultActor : MonoBehaviour, IActor
+public class DefaultActor : MonoBehaviour, IActor, IEventListener
 {
     public int actorLevel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
     public IActor target { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
-    public MovementController movementController => throw new System.NotImplementedException();
+    public MovementController movementController
+    {
+        get;
+        private set;
+    }
 
+    public void Start()
+    {
+        movementController = GetComponent<MovementController>();
+        EventSystem.AddEventListener(EventSystem.EventChannel.player, EventSystem.EventSubChannel.input, this);
+        
+    }
     public bool IsPlayer()
     {
-        throw new System.NotImplementedException();
+        if(this is PlayerActor)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void PickUpItem(object item)
@@ -22,5 +40,20 @@ public class DefaultActor : MonoBehaviour, IActor
     public void UseItem(object item)
     {
         throw new System.NotImplementedException();
+    }
+
+    public void AcceptEvent(IEvent e)
+    { 
+        if (IsPlayer()) {
+            if (e is MoveInputEvent mie)
+            {
+                Vector2 nextLocation = (Vector2) mie.ray.origin;
+                movementController.Move(nextLocation);
+            }
+            if (e is AttackInputEvent aie)
+            {
+                //TODO
+            }
+        }
     }
 }
--- TouchCrawlerUnity/Assets/Scripts/Actor/MovementController.cs
+++ TouchCrawlerUnity/Assets/Scripts/Actor/MovementController.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    Rigidbody2D body
-    {
-        get;
-    }
-    Vector2 nextLocation;
+    Rigidbody2D body;
+    Vector2 destination;
+    Vector2 velocity;
+    Vector2 maxVelocity;
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GetComponent<Rigidbody2D>();
+        velocity = Vector2.zero;
+        destination = body.position;
+        maxVelocity = new Vector2(3f, 3f);
     }
 
-    void Move(Vector2 nextLocation)
+    void Update()
     {
-        this.nextLocation = nextLocation;
+        //Debug.Log(nextLocation);
+        //Debug.Log(velocity);
+        if (IsAtDestination())
+        {
+            velocity = Vector2.zero;
+        }
+
+        body.velocity = this.velocity;
+
+    }
+    public void Move(Vector2 nextLocation)
+    {
+        this.destination = nextLocation;
+        Vector2 currentLocation = body.position;
+        velocity = new Vector2((nextLocation.x - currentLocation.x), (nextLocation.y - currentLocation.y));
+        velocity.Normalize();
+        velocity.Scale(maxVelocity);
     }
 
     void Stop()
     {
-
+        body.velocity = Vector2.zero;
     }
-    // Update is called once per frame
-    void Update()
+    
+    private bool IsAtDestination()
     {
-
+        bool isAtX = (Mathf.Round(body.position.x * 10) / 10.0) == (Mathf.Round(destination.x * 10) / 10.0);
+        bool isAtY = (Mathf.Round(body.position.y * 10) / 10.0) == (Mathf.Round(destination.y * 10) / 10.0);
+        return isAtX && isAtY;
     }
+
 }
