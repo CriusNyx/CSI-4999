118bef0af80848c53c1e841b3abfdcc13559e2b0
rclough158 <35506949+rclough158@users.noreply.github.com>
2020-01-30 16:22:59 -0500

    Door Flicker
    Programmatically adjust alpha of lit graphic to simulate flickering light
    Gradual initial lighting of door acheived in the same way

---
+++ TouchCrawlerUnity/Assets/Scripts/MapControl/DoorFlicker.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorFlicker : MonoBehaviour
+{
+    private Tilemap overlay;
+    private bool on;
+    // Start is called before the first frame update
+    void Start()
+    {
+        on = false;
+        overlay = GetComponent<Tilemap>();
+        Light();
+    }
+
+    void Flicker()
+    {
+        StartCoroutine(WaitFlicker());
+        StopCoroutine(WaitFlicker());
+    }
+
+    IEnumerator WaitFlicker()
+    {
+        yield return new WaitForSeconds(0.05f);
+        Debug.Log("test WaitLight()");
+        var temp = overlay.color;
+        temp.a = Random.Range(.5f, 1);
+        overlay.color = temp;
+
+        if (on)
+        {
+            Flicker();
+        }
+    }
+
+
+
+    void Light()
+    {
+            StartCoroutine(WaitLight());
+            StopCoroutine(WaitLight());
+    }
+
+    IEnumerator WaitLight()
+    {
+        yield return new WaitForSeconds(0.01f);
+        Debug.Log("test WaitLight()");
+        var temp = overlay.color;
+        temp.a += .05f;
+        overlay.color = temp;
+
+        if(temp.a <= .95)
+        {
+            Light();
+        } else
+        {
+            on = true;
+            Flicker();
+        }
+    }
+
+}
