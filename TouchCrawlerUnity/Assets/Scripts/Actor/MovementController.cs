a0be35da0a1620d05541209f836205540ae62b86
zzweber1 <35668141+zzweber1@users.noreply.github.com>
2020-01-31 16:37:19 -0500

    Collision Detection and Smooth Velocity

--- TouchCrawlerUnity/Assets/Scripts/Actor/MovementController.cs
+++ TouchCrawlerUnity/Assets/Scripts/Actor/MovementController.cs
@@ -5,48 +5,62 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     Rigidbody2D body;
+    BoxCollider2D collider;
     Vector2 destination;
     Vector2 velocity;
-    Vector2 maxVelocity;
+    public Vector2 maxVelocity;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
         velocity = Vector2.zero;
         destination = body.position;
         maxVelocity = new Vector2(3f, 3f);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        //Debug.Log(nextLocation);
-        //Debug.Log(velocity);
         if (IsAtDestination())
         {
-            velocity = Vector2.zero;
+            Stop();
         }
 
-        body.velocity = this.velocity;
+        var delta = (destination - body.position);
+        velocity = delta.normalized;
+        velocity.Scale(maxVelocity);
+        var velocityPerFrame = velocity * Time.fixedDeltaTime;
+        if(delta.magnitude < velocityPerFrame.magnitude)
+        {
+            body.velocity = delta / Time.fixedDeltaTime;
+        }
+        else
+        {
+            body.velocity = velocity;
+        }
 
     }
     public void Move(Vector2 nextLocation)
     {
         this.destination = nextLocation;
-        Vector2 currentLocation = body.position;
-        velocity = new Vector2((nextLocation.x - currentLocation.x), (nextLocation.y - currentLocation.y));
-        velocity.Normalize();
-        velocity.Scale(maxVelocity);
     }
 
     void Stop()
     {
-        body.velocity = Vector2.zero;
+        destination = body.position;
+      
     }
-    
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision Detected");
+        Stop();
+    }
+
     private bool IsAtDestination()
     {
-        bool isAtX = (Mathf.Round(body.position.x * 10) / 10.0) == (Mathf.Round(destination.x * 10) / 10.0);
-        bool isAtY = (Mathf.Round(body.position.y * 10) / 10.0) == (Mathf.Round(destination.y * 10) / 10.0);
+        bool isAtX = (Mathf.Round(body.position.x * 5) / 5.0) == (Mathf.Round(destination.x * 5) / 5.0);
+        bool isAtY = (Mathf.Round(body.position.y * 5) / 5.0) == (Mathf.Round(destination.y * 5) / 5.0);
         return isAtX && isAtY;
     }
 
