17fa363e075dc0f6962a2d5f239a4efa4fc974e1
CriusNyx <RJReynoldsW@gmail.com>
2020-02-02 12:12:54 -0500

    Projectile Factory

---
+++ TouchCrawlerUnity/Assets/Scripts/WeaponSystem/ProjectileFactory.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Util;
+using Assets.Scripts.WeaponSystem;
+using Assets.WeaponSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFactory : WeaponComponent
+{
+    public override ComponentType componentType => ComponentType.ProjectileFactory;
+
+    public GameObject prefab;
+
+    public override CreateProjectileResult CreateProjectile(Weapon weapon, IWeaponTarget target, Vector3 position, Quaternion rotation, Vector3 direction, CreateProjectileResult result)
+    {
+        var instance = GameObjectFactory.Instantiate(prefab, position, rotation);
+        var projectile = instance.GetComponent<IProjectile>();
+
+        if(projectile != null)
+        {
+            Vector2 velocity = target.gameObject.transform.position - weapon.gameObject.transform.position;
+            projectile.Initialize(weapon, target, direction, position);
+            result.Add(projectile);
+        }
+
+        return result;
+    }
+}
---
+++ TouchCrawlerUnity/ProjectileFactory.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.WeaponSystem;
+using Assets.WeaponSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFactory : WeaponComponent
+{
+    public override CreateProjectileResult CreateProjectile(Weapon weapon, IWeaponTarget target, Vector3 position, Quaternion rotation, Vector3 direction, CreateProjectileResult result)
+    {
+
+    }
+}
