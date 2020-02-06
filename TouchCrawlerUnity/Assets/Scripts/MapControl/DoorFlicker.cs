6df2769a1a003ca9f3cab42cd7bc250f55e9b078
rclough158 <35506949+rclough158@users.noreply.github.com>
2020-02-06 17:29:46 -0500

    added comments

---
+++ TouchCrawlerUnity/Assets/Scripts/MapControl/DoorFlicker.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorFlicker : MonoBehaviour
+{
+    private Tilemap overlay;
+    private bool on;
+    
+    void Start()
+    {
+        on = false;
+        overlay = GetComponent<Tilemap>();
+        LightOn();
+    }
+
+    //Set graphic alpha to 0
+    //likely unnecessary, we'll just deactivate/reactivate object as needed
+    void LightOff()
+    {
+        on = false;
+        var temp = overlay.color;
+        temp.a = 0;
+        overlay.color = temp;
+    }
+
+    void LightOn()
+    {
+        on = false; //here for relighting, stops Flicker
+        StartCoroutine(WaitLight());
+        StopCoroutine(WaitLight());
+    }
+
+    //pause .01 seconds, increase graphic alpha by 5%
+    //loops until alpha is 100% and the door is "lit"
+    //start flicker animation
+    IEnumerator WaitLight()
+    {
+        yield return new WaitForSeconds(0.01f);
+        var temp = overlay.color;
+        temp.a += .05f;
+        overlay.color = temp;
+
+        if(temp.a <= .95)
+        {
+            LightOn();
+        } else
+        {
+            on = true;
+            Flicker();
+        }
+    }
+
+
+
+    void Flicker()
+    {
+        StartCoroutine(WaitFlicker());
+        StopCoroutine(WaitFlicker());
+    }
+
+    //continues forever, unless the door light goes off
+    //ex: when the door is closed, flip to on = false
+    //randomly pick a value between 50% and 100% for alpha every .05 seconds
+    IEnumerator WaitFlicker()
+    {
+        yield return new WaitForSeconds(0.05f);
+        var temp = overlay.color;
+        temp.a = Random.Range(.5f, 1);
+        overlay.color = temp;
+
+        if (on)
+        {
+            Flicker();
+        }
+    }
+
+}
---
+++ TouchCrawlerUnity/Assets/Scripts/MapControl/RoomController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    private GameObject[] doorList = new GameObject[5];
+    public bool cleared;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cleared = false;
+        for (int i = 0; i < 5; i++)
+        {
+            doorList[i] = this.transform.GetChild(i).gameObject;
+            //west = 0, north = 1, east = 2, south = 3, down = 4
+        }
+    }
+
+    void CheckNeighbors()
+    {
+        //check if neighboring cells have rooms, set doors active
+    }
+
+
+    void ToggleDoorOpen(bool set)
+    {
+        
+        if (set)
+        {
+            //true = open
+            foreach (GameObject door in doorList)
+            {            
+                //test if it doesn't like inactive doors    
+                door.transform.GetChild(2).gameObject.SetActive(false); //turn door OFF
+                door.transform.GetChild(0).gameObject.SetActive(true); //turn trigger collider ON
+                //cleared is only open condition?
+                cleared = true;
+            }
+        }else
+        {
+            //false = close
+            foreach (GameObject door in doorList)
+            {
+                door.transform.GetChild(2).gameObject.SetActive(true); //turn door ON
+                door.transform.GetChild(0).gameObject.SetActive(false); //turn trigger collider OFF
+                //check if lit, record, relight after?
+            }
+        }
+        
+    }
+
+    void ToggleDoorLit(bool set, int id)
+    {
+        //true = on; room explored
+        //false = off; room not explored
+        if (set)
+        {
+            doorList[id].transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            doorList[id].transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+    }
+
+}
