11718a03f6c01db9230c5f06300d7c55933fd0a2
CriusNyx <RJReynoldsW@gmail.com>
2020-02-09 14:46:30 -0500

    Added Projectile Definitions

--- TouchCrawlerUnity/Assets/Scripts/WeaponSystem/ProjectileFactory.cs
+++ TouchCrawlerUnity/Assets/Scripts/WeaponSystem/ProjectileFactory.cs
@@ -9,11 +9,11 @@ public class ProjectileFactory : WeaponComponent
 {
     public override ComponentType componentType => ComponentType.ProjectileFactory;
 
-    public GameObject prefab;
+    //public GameObject prefab;
 
-    public override CreateProjectileResult CreateProjectile(Weapon weapon, IWeaponTarget target, Vector3 position, Quaternion rotation, Vector3 direction, CreateProjectileResult result)
+    public override CreateProjectileResult CreateProjectile(Weapon weapon, IWeaponTarget target, Vector3 position, Quaternion rotation, Vector3 direction, BulletSpawnInfo spawnInfo, CreateProjectileResult result)
     {
-        var instance = GameObjectFactory.Instantiate(prefab, position, rotation);
+        var instance = GameObjectFactory.Instantiate(spawnInfo.prefabToSpawn, position, rotation);
         var projectile = instance.GetComponent<IProjectile>();
 
         if(projectile != null)
@@ -22,7 +22,7 @@ public class ProjectileFactory : WeaponComponent
             var owner = weapon.owner;
             var actor = owner.actor;
             var targetType = actor.AttackWeaponTargetType;
-            projectile.Initialize(weapon, target, direction, position, weapon.owner.actor.AttackWeaponTargetType);
+            projectile.Initialize(weapon, target, direction, position, spawnInfo.color, spawnInfo.speedModifier, weapon.owner.actor.AttackWeaponTargetType);
             result.Add(projectile);
         }
 
