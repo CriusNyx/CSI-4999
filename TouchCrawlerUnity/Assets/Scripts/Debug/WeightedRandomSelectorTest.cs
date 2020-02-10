cda457b894e75aebaf3490c78aa73fbe24cc5c81
WINDOWS-US54VFC\RJ 1050 <rjreynoldsw@gmail.com>
2020-02-10 14:19:06 -0500

    Random Selector Test

--- TouchCrawlerUnity/Assets/Scripts/Debug/WeightedRandomSelectorTest.cs
+++ TouchCrawlerUnity/Assets/Scripts/Debug/WeightedRandomSelectorTest.cs
@@ -10,17 +10,26 @@ public class WeightedRandomSelectorTest : MonoBehaviour
     void Start()
     {
         weightedRandomSelector.Add(0, 1f);
-        weightedRandomSelector.Add(1, 0f);
+        weightedRandomSelector.Add(1, 1f);
+        weightedRandomSelector.Add(2, 1f);
+        weightedRandomSelector.Add(3, 1f);
 
         Dictionary<int, int> counter = new Dictionary<int, int>();
+
         counter[0] = 0;
         counter[1] = 0;
+        counter[2] = 0;
+        counter[3] = 0;
+
         for(int i = 0; i < 10000; i++)
         {
             counter[weightedRandomSelector.Select(Random.value)]++;
         }
 
-        Debug.Log("0 count = " + counter[0] + ", 1 count = " + counter[1]);
+        for(int i = 0; i < 4; i++)
+        {
+            Debug.Log(i + " count = " + counter[i]);
+        }
     }
 
     // Update is called once per frame
