67b26a6003709a75d369d6c507a6d7142282628f
Sam <stahiri719@gmail.com>
2020-02-10 01:30:39 -0500

    Nicer Screen Shake + Hit Pause
    Screen shake now has translational, rotational movement, and Perlin Noise. Standard Hit Pause implementation.

---
+++ TouchCrawlerUnity/Assets/Scripts/Juice/HitPauseController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPauseController : MonoBehaviour
+{
+    [Range(0f,1.5f)]
+    public float duration = 1f;
+    float pendingFreezeDuration = 0f;
+    bool isFrozen = false;
+
+    void Update()
+    {
+        if (pendingFreezeDuration < 0 && !isFrozen)
+        {
+            StartCoroutine(DoFreeze());
+        }
+    }
+
+    public void Freeze()
+    {
+        pendingFreezeDuration = duration;
+    }
+
+    IEnumerator DoFreeze()
+    {
+        isFrozen = true;
+        var original = Time.timeScale;
+        Time.timeScale = 0f;
+
+        yield return new WaitForSecondsRealtime(duration);
+
+        Time.timeScale = original;
+        pendingFreezeDuration = 0;
+        isFrozen = false;
+
+    }
+}
---
+++ TouchCrawlerUnity/Assets/Scripts/Juice/ScreenShakeController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeController : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Length of shake effect
+    public float shakeTime = 0.5f;
+    // Magntiude of shake effect
+    [Range(0f, 1f)]
+    public float shakeMagnitude = 0.4f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Shake(shakeMagnitude, shakeTime));
+        }
+    }
+
+    void Awake()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    public IEnumerator Shake(float trauma, float length)
+    {
+        this.shakeMagnitude = trauma;
+        this.shakeTime = length;
+
+        // Store normal position + rotation before we start shaking
+        Vector3 originalPosition = transform.localPosition;
+        Quaternion originalRotation = transform.localRotation;
+
+        float elapsed = 0.0f;
+
+        if (trauma > 0)
+        {
+            while (elapsed < length)
+            {
+                // Camera Shake = trauma^2
+                // Shake Angle = maxAngle * trauma * RandomFloatFromNegOneToOne
+                // OffsetX = maxOffset * trauma * RandomFloatFromNegOneToOne
+                // OffsetY = maxOffset * trauma * RandomFloatFromNegOneToOne
+
+                float offsetXYZ = Random.Range(-1f, 1f) * trauma;
+                float cameraShake = Mathf.Pow(trauma, 2);
+                
+                // Note: The recurring number 25 is the "frequency" of the Perlin Noise
+                // Many things can be turned into component variables, but I left it out for simplicity's sake -Sam
+                transform.localPosition = new Vector3(
+                    (Mathf.PerlinNoise(offsetXYZ, Time.time * 25) * 2 - 1),
+                    (Mathf.PerlinNoise(offsetXYZ + 1, Time.time * 25) * 2 - 1),
+                    (Mathf.PerlinNoise(offsetXYZ + 2, Time.time * 25) * 2 - 1)) * cameraShake;
+
+                transform.localRotation = Quaternion.Euler(new Vector3(
+                    (Mathf.PerlinNoise(offsetXYZ + 3, Time.time * 25) * 2 - 1),
+                    (Mathf.PerlinNoise(offsetXYZ + 4, Time.time * 25) * 2 - 1),
+                    (Mathf.PerlinNoise(offsetXYZ + 5, Time.time * 25) * 2 - 1)) * cameraShake);
+
+                elapsed += Time.deltaTime;
+                
+                yield return null;
+            }
+
+            // Go back to normal position + rotation
+            transform.localPosition = originalPosition;
+            transform.localRotation = originalRotation;
+        }
+    }
+}
