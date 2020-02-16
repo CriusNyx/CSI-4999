e281210b4de0c530a81a8e9fdb0092f6344ef462
Sam <stahiri719@gmail.com>
2020-02-16 05:14:12 -0500

    Pick up + Store items
    Need to add components to stored items - right now it's just the icon.

---
+++ TouchCrawlerUnity/Assets/Scripts/Inventory/PickUpItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpItem : MonoBehaviour
+{
+    private Inventory inventory;
+    private GameObject[] itemSlots;
+    private SpriteRenderer itemIcon;
+
+    private void Start()
+    {
+        // Get Player prefab, get ItemSlot prefabs
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        itemSlots = GameObject.FindGameObjectsWithTag("Item Slot");
+    }
+
+    // Checks to see if player collides with item
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.items.Length; i++)
+            {
+                if (inventory.items[i] == 0)
+                {
+                    // Store item into inventory
+                    inventory.items[i] = 1;
+
+                    // Set the image and color, if applicable, of the item picked up
+                    Image image = itemSlots[i].transform.Find("ItemIcon").GetComponent<Image>();
+
+                    if (!image.enabled)
+                    {
+                        image.enabled = true;
+
+                        itemIcon = this.GetComponent<SpriteRenderer>();
+                        image.sprite = itemIcon.sprite;
+                        image.color = itemIcon.color;
+                    }
+                    else
+                    {
+                        itemIcon = gameObject.GetComponent<SpriteRenderer>();
+                        image.sprite = itemIcon.sprite;
+                        image.color = itemIcon.color;
+                    }
+
+                    // Remove item from overworld
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
