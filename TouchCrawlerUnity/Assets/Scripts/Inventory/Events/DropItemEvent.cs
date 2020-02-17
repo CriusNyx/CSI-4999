e8a9dc837474c948732259e2ab676f7fdcfbd190
CriusNyx <RJReynoldsW@gmail.com>
2020-02-17 18:27:56 -0500

    Added drag and stop support

---
+++ TouchCrawlerUnity/Assets/Scripts/Inventory/Events/DropItemEvent.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemEvent : IEvent
+{
+    public readonly Item item;
+
+    public DropItemEvent(Item item)
+    {
+        this.item = item;
+    }
+}
