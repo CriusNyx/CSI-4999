6fb33008104dc20dfde70345a9c6d8f3f01bfde5
zzweber1 <35668141+zzweber1@users.noreply.github.com>
2020-02-19 18:18:04 -0500

    Increase flexibility of enemy AI

---
+++ TouchCrawlerUnity/Assets/Scripts/Actor/EnemyAI/EnemyAIController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.WeaponSystem;
+using Assets.WeaponSystem;
+
+[RequireComponent(typeof(AIBehaviorAttack))]
+[RequireComponent(typeof(AIBehaviorWander))]
+[RequireComponent(typeof(AIBehaviorPatrol))]
+public class EnemyAIController : MonoBehaviour
+{
+    //Behavior variables
+
+    public int stopDuration;
+    private int stopTime;
+
+
+    //Actor components
+    private AIBehaviorPatrol aIBehaviorPatrol;
+    private AIBehaviorWander aIBehaviorWander;
+    private AIBehaviorAttack aIBehaviorAttack;
+    private Rigidbody2D body;
+    private MovementController movementController;
+    private NPCActor npcActor;
+    
+
+    void Start()
+    {
+        movementController = GetComponent<MovementController>();
+        stopTime = 0;
+        aIBehaviorAttack = GetComponent<AIBehaviorAttack>();
+        aIBehaviorPatrol = GetComponent<AIBehaviorPatrol>();
+        aIBehaviorWander = GetComponent<AIBehaviorWander>();
+        npcActor = GetComponent<NPCActor>();
+        body = GetComponent<Rigidbody2D>();
+
+        if (aIBehaviorWander.enabled && aIBehaviorPatrol.enabled && aIBehaviorAttack.enabled)
+        {
+            aIBehaviorWander.enabled = true;
+            aIBehaviorPatrol.enabled = false;
+            aIBehaviorAttack.enabled = false;
+        }
+        if((aIBehaviorWander.enabled || aIBehaviorPatrol.enabled) && aIBehaviorAttack.enabled)
+        {
+            aIBehaviorAttack.enabled = false;
+        }
+        if (aIBehaviorWander.enabled && aIBehaviorPatrol.enabled)
+        {
+            aIBehaviorPatrol.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (WasAttackedYet())
+        {
+            StartAttacking();
+        }
+    }
+    
+
+
+
+
+
+    private bool WasAttackedYet()
+    {
+        return npcActor.attacker != null;
+    }
+
+    private void StartAttacking()
+    {
+        aIBehaviorAttack.enabled = true;
+        aIBehaviorPatrol.enabled = false;
+        aIBehaviorWander.enabled = false;
+    }
+}
