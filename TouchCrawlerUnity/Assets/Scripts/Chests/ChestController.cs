07604f0ce043d97f2e9830486b49ff93926f8ab9
zzweber1 <35668141+zzweber1@users.noreply.github.com>
2020-03-09 14:30:09 -0400

    Fixed Chests

--- TouchCrawlerUnity/Assets/Scripts/Chests/ChestController.cs
+++ TouchCrawlerUnity/Assets/Scripts/Chests/ChestController.cs
@@ -3,14 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestController : MonoBehaviour, IWeaponTarget
+public class ChestController : MonoBehaviour
 {
     public IActor actor => throw new System.NotImplementedException();
 
-    public MonoBehaviour monoBehaviour
+    private DefaultActor chestActor;
+
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite midFrame;
+    public Sprite endFrame;
+
+    Vector3 dropPosition;
+
+    public bool wasOpened
     {
-        get => this;
+        get;
+        private set;
     }
+    private bool wasDropped;
 
     DropTable dropTable;
 
@@ -18,18 +29,33 @@ public class ChestController : MonoBehaviour, IWeaponTarget
     void Start()
     {
         dropTable = Resources.Load<DropTable>("ProceduralGenerationSystem/DropTable");
+        chestActor = GetComponent<ChestActor>();
+        wasOpened = false;
+        wasDropped = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        dropPosition = chestActor.transform.position - new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (wasOpened && !wasDropped)
+        {
+            spriteRenderer.sprite = endFrame;
+            Instantiate(dropTable.GetRandom(), dropPosition, Quaternion.identity);
+            wasDropped = true;
+        }
+        if (WasAttacked() && !wasOpened)
+        {
+            Debug.Log("Chest was opened");
+            wasOpened = true;
+            spriteRenderer.sprite = midFrame;
+        }
+ 
     }
 
-    public bool DoDamage(Damage damage)
+    bool WasAttacked()
     {
-        Debug.Log("Chest was attacked");
-        Instantiate(dropTable.GetRandom(), Vector3.zero, Quaternion.identity);
-        return true;
+        return chestActor.wasAttacked;
     }
 }
