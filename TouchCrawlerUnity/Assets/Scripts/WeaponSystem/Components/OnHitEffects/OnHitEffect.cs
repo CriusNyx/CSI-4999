c092c04baba3deb150919f828510e56a42ed616c
Justin Scott <justinjonscott@gmail.com>
2020-03-11 17:00:49 -0400

    Update OnHitEffect.cs
    made abstract to properly implement other onhiteffects

--- TouchCrawlerUnity/Assets/Scripts/WeaponSystem/Components/OnHitEffects/OnHitEffect.cs
+++ TouchCrawlerUnity/Assets/Scripts/WeaponSystem/Components/OnHitEffects/OnHitEffect.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.WeaponSystem.Components.OnHitEffects {
-    public class OnHitEffect : WeaponComponent
+    public abstract class OnHitEffect : WeaponComponent
     {
         public override ComponentType componentType => ComponentType.OnHitEffect;
 
@@ -21,21 +21,6 @@ namespace Assets.Scripts.WeaponSystem.Components.OnHitEffects {
             }
             return result;
         } 
-        private bool ApplyDebuff(IWeaponTarget target) {
-            //TODO: Debuffs
-            //Get Target Actor
-            //Apply debuff to target
-            //Return based on success of applying debuff
-            //throw new System.NotImplementedException();
-            return false;
-        }
 
-        protected virtual void ApplyKnockback(Weapon weapon, IWeaponTarget target) {
-            //throw new System.NotImplementedException();
-        }
-
-        protected virtual void SpawnAnimation(Vector2 location) {
-            //throw new System.NotImplementedException();
-        }
     }
 }
