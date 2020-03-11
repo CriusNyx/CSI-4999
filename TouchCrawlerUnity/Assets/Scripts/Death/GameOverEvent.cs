4aedd9a8e0a157df02a73703f08c9c342a941cbd
Justin Scott <justinjonscott@gmail.com>
2020-03-11 17:54:46 -0400

    Update GameOverEvent.cs
    still need to work on

--- TouchCrawlerUnity/Assets/Scripts/Death/GameOverEvent.cs
+++ TouchCrawlerUnity/Assets/Scripts/Death/GameOverEvent.cs
@@ -4,10 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Death
 {
     class GameOverEvent : IEvent
     {
+    }
+
+    public class GameOverEventListener : IEventListener {
+
+        public void AcceptEvent(IEvent e){
+            if(e is GameOverEvent myEvent){
+                DoSomething(myEvent);
+            }
+        }
     }
 }
