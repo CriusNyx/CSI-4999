400b5072a6d5872062c2666c6f96c8c7e82f0182
Justin Scott <justinjonscott@gmail.com>
2020-03-27 14:47:21 -0400

    working script

--- TouchCrawlerUnity/Assets/Scripts/Leaderboard/ScoreHandler.cs
+++ TouchCrawlerUnity/Assets/Scripts/Leaderboard/ScoreHandler.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class ScoreHandler : MonoBehaviour
 {
+
+    public Text leaderboard;
+    public int start = 0;
+    public int end = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +21,7 @@ public class ScoreHandler : MonoBehaviour
     // Update is called once per frame
     IEnumerator Request()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://touchcrawler.appspot.com/topscores?start=0&end=2");
+        UnityWebRequest www = UnityWebRequest.Get(string.Format("http://touchcrawler.appspot.com/topscores?start={0}&end={1}", start.ToString(), end.ToString()));
         yield return www.SendWebRequest();
 
         if (www.isNetworkError || www.isHttpError)
@@ -23,7 +30,21 @@ public class ScoreHandler : MonoBehaviour
         }
         else {
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            ScoreResults results = JsonConvert.DeserializeObject<ScoreResults>(www.downloadHandler.text);
+            if (results.error == null)
+            {
+                string info = "";
+                int i = 1;
+                foreach (JArray score in results.data)
+                {
+                    info += string.Format("{0}) Score: {1} || Name: {2} || Time: {3} \n",i, score[0], score[1], score[2]);
+                    i++;
+                }
+                this.leaderboard.text = info;
+            }else
+            {
+                Debug.Log("Error in HTTPRequest");
+            }
 
         }
     }
--- TouchCrawlerUnity/Assets/Scripts/Leaderboard/ScoreResults.cs
+++ TouchCrawlerUnity/Assets/Scripts/Leaderboard/ScoreResults.cs
@@ -1,14 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 [System.Serializable]
 public class ScoreResults
 {
     public object error { get; set; }
     public string timestamp { get; set; }
-    public List<List<object>> data { get; set; }
+    public object[] data { get; set; }
 
-    public static ScoreResults CreateFromJSON(string jsonString)
-    {
-        return JsonUtility.FromJson<ScoreResults>(jsonString);
-    }
 }
 
 
