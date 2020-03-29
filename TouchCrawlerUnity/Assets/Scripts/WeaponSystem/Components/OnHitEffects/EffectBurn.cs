30e386614855df42ed5aa44d07d3de0985918317
Justin Scott <justinjonscott@gmail.com>
2020-03-29 10:19:41 -0400

    Update EffectBurn.cs

--- TouchCrawlerUnity/Assets/Scripts/WeaponSystem/Components/OnHitEffects/EffectBurn.cs
+++ TouchCrawlerUnity/Assets/Scripts/WeaponSystem/Components/OnHitEffects/EffectBurn.cs
@@ -19,7 +19,7 @@ namespace Assets.Scripts.WeaponSystem.Components.OnHitEffects {
         float coefficient = 0.1f;
         int burnTime = 6;
 
-        int SecondsPerTick = 2;
+        int ticks = 3;
 
         float amount = 0f;
 
@@ -35,22 +35,11 @@ namespace Assets.Scripts.WeaponSystem.Components.OnHitEffects {
             this.amount = spellPower.BaseValue;
             this.amount += spellPower.CalculateStatValue() * coefficient;
 
-            StartCoroutine(BurnTarget());
+            EffectDot dot = new EffectDot(target, burnTime, ticks, new SpellDamage(amount/ticks));
 
             result.applyEffects = true;
 
             return result;
-        }
-        IEnumerator BurnTarget() {
-
-            for (int i = 0; i < (burnTime/SecondsPerTick); i++){
-
-                //TODO: Spawn Fire on targets location
-
-                yield return new WaitForSeconds(SecondsPerTick);
-                SpellDamage damage = new SpellDamage(amount/(burnTime/SecondsPerTick));
-                target.DoDamage(damage);
-            }
         }
     }
 
