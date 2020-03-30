84cb9563ac71a0ab22ba06a869967f5b6ad1d55b
Justin Scott <justinjonscott@gmail.com>
2020-03-30 12:43:08 -0400

    leaderboard button

---
+++ TouchCrawlerUnity/Assets/Scripts/ButtonHandlers/TestButtonHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using System.Collections;
+
+public class TestButtonHandler : MonoBehaviour
+{
+    public SceneField MainMenuScene;
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(MainMenuScene, LoadSceneMode.Single);
+    }
+}
