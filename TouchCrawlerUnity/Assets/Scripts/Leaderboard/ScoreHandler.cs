fbfb63ac18b4cad7673770b14d7821ce7ece57c8
Justin Scott <justinjonscott@gmail.com>
2020-04-11 05:02:46 -0400

    leaderboard polishing

--- TouchCrawlerUnity/Assets/Scripts/Leaderboard/ScoreHandler.cs
+++ TouchCrawlerUnity/Assets/Scripts/Leaderboard/ScoreHandler.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -33,11 +35,12 @@ public class ScoreHandler : MonoBehaviour
             ScoreResults results = JsonConvert.DeserializeObject<ScoreResults>(www.downloadHandler.text);
             if (results.error == null)
             {
-                string info = "";
+                string info = "";// string.Format("{0,-15} {1,-15} {2,-8} {3} \n","Place","Username","Score","Date");
                 int i = 1;
                 foreach (JArray score in results.data)
                 {
-                    info += string.Format("{0}) Score: {1} || Name: {2} || Time: {3} \n",i, score[0], score[1], score[2]);
+                    DateTime date = new DateTime(1970,1,1,0,0,0, DateTimeKind.Utc);
+                    info += string.Format("{0,-16} {2,-22} {1,-17} {3} \n", i, score[0], score[1], date.AddSeconds(Convert.ToDouble(Regex.Replace((string)score[2], "[^0-9]", ""))).ToString("dd-MM-yyyy"));
                     i++;
                 }
                 this.leaderboard.text = info;
