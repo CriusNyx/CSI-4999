fca43334109d8c95d23f8f1e5562d16d33d3a001
Justin Scott <justinjonscott@gmail.com>
2020-04-12 15:24:44 -0400

    time for some scores

---
+++ TouchCrawlerUnity/Assets/Scripts/Death/IncreaseScore.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Death;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Death
+{
+    public class IncreaseScore : OnDieEffect
+    {
+        public int value = 1;
+        public override void OnDie(IActor actor)
+        {
+            if (!actor.IsPlayer())
+            {
+                UserSettings.score += value;
+            }
+        }
+
+    }
+}
