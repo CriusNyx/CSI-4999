53c9f22fb0dab4a4bb9e575bbda3df760c973a8c
Justin Scott <justinjonscott@gmail.com>
2020-04-12 04:35:24 -0400

    settings file

---
+++ TouchCrawlerUnity/Assets/Scripts/Settings/ProfileSettings.cs
@@ -0,0 +1,11 @@
+﻿using Firebase.Auth;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileSettings : MonoBehaviour
+{
+    public static float volumeMusic;
+    public static float volumeSfx;
+    public static string token;
+}
